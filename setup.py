cfb9528c2c9949e4f3b91c46b712c5cbbbae9591
aspangher <aspangher@bloomberg.net>
2021-04-28 15:55:35 -0700

    e

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name='get_edit_diffs',
@@ -8,7 +8,7 @@ setup(
     author='Alex Spangher',
     author_email='aspangher@bloomberg.net',
     license='',
-    packages=['extract_newsarchive'],
+    packages=find_packages(),
     install_requires=open('requirements.txt').read().split('\n'),
     entry_points={
       'console_scripts': [
