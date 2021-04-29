a0780f0798c606cec365b0385337b9aa49607dd4
aspangher <aspangher@bloomberg.net>
2021-04-29 16:12:59 -0700

    e

---
+++ spark_processing_scripts/run.sh
@@ -0,0 +1,26 @@
+# cluster ds-ob-dev02
+HADOOP_IDENTITY="aspangher-cluster-test"
+BCS_IDENTITY="aspangher-bcs-test"
+GIT_IDENTITY="spectro-oauth-aspangher"
+NAMESPACE="aspangher"
+
+# 4 for BB authored news, 6 for web-scraped content
+#for PVF_LEVEL in 4 6; do
+katie spark run \
+    --namespace ${NAMESPACE} \
+    --identities ${HADOOP_IDENTITY} ${BCS_IDENTITY} ${GIT_IDENTITY} \
+    --job-name sentence-parse \
+    --python-mode \
+    --spark-framework spark-2.4-python-3.7 \
+    --python-package-uris git+https://bbgithub.dev.bloomberg.com/aspangher/edit-project \
+    --py-uri /job/.local/bin/run_pyspark.py \
+    --cluster-size Custom \
+    --worker-count 50 \
+    --worker-cores 5 \
+    --worker-memory 20G \
+    --driver-memory 20G \
+    --driver-cores 5 \
+    --sync-launch tail \
+    -- \
+      --db_name nyt \
+      --num_files 500
