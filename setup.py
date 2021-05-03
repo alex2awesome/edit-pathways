b9c29473522bd7358e58527d923c2372203f2820
aspangher <aspangher@bloomberg.net>
2021-05-02 18:38:25 -0700

    e

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setup(
     install_requires=open('requirements.txt').read().split('\n'),
     entry_points={
       'console_scripts': [
-          'run_pyspark.py = spark_processing_scripts.runner_script:main'
+          'run_pyspark.py = spark_processing_scripts.runner:main'
       ]
     },
     zip_safe=False
