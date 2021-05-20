11ad6cf356d3a927e6c86370b91b97ff0aaa152c
aspangher <aspangher@bloomberg.net>
2021-05-20 01:30:00 -0700

    e

--- spark_processing_scripts/run_main_pluslab.sh
+++ spark_processing_scripts/run_main_pluslab.sh
@@ -3,7 +3,7 @@ DB_NAME="guardian"
 
 # 4 for BB authored news, 6 for web-scraped content
 #for PVF_LEVEL in 4 6; do
-python runner.py \
+python3 runner.py \
       --db_name $DB_NAME \
       --num_files 500 \
       --continuous
