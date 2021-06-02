cf12114b2ada550cef69c3143f8bcf4ae3790518
aspangher <aspangher@bloomberg.net>
2021-06-02 13:18:28 -0700

    e

--- spark_processing_scripts/runner.py
+++ spark_processing_scripts/runner.py
@@ -73,7 +73,7 @@ def main():
     s3_path = sug.s3_output_dir_main if not args.split_sentences else sug.s3_output_dir_sentences
     file_count = len(sug.get_files(s3_path, args.db_name, sug.csv_pat)) if not args.split_sentences else len(sug.get_files(s3_path, args.db_name, sug.pkl_pat))
 
-    print('FILES FOUND: %s' % len(file_count))
+    print('FILES FOUND: %s' % file_count)
     print('NUM IDs FOUND: %s' % len(prefetched_entry_ids))
 
     # loop spark job
